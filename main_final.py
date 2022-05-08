2169359e529f5e7b556ccc06adc1a581dd99c4e6
IRiS_CiTy <iris_city@outlook.com>
2022-05-08 03:48:59 -0500

    doctest update

--- main_final.py
+++ main_final.py
@@ -1879,17 +1879,17 @@ def show_dict_report(report_name: str, winner_dict: dict, total_count_dict: dict
     print("Skill Name{}Win Fights{}Total Occurrence{}Winning Rate{}Occurrence Rate(Two sides count separately)"
           .format(' ' * (25 - len('Skill Name')), ' ' * (15 - len('Win Fights')),
                   ' ' * (20 - len('Total Occurrence')),
-                  ' ' * (27 - len('Occurrence Rate(Two sides count separately)'))))
+                  ' ' * (20 - len('Winning Rate)'))))
     for skill in winner_dict.keys():
         rate_occ_two_side_total = round(int(total_count_two_sides_dict[skill]) / (loop_times * 2) * 100, 2)
         rate_winner_side = round(int(winner_dict[skill]) / total_count_dict[skill] * 100, 2)
         dict_for_plot[skill] = rate_winner_side
-        print("{}{}{}{}{}{}{}%"
+        print("{}{}{}{}{}{}{}%{}{}%"
               .format(skill, ' ' * (25 - len(skill)),
                       winner_dict[skill], ' ' * (15 - len(str(winner_dict[skill]))),
                       total_count_dict[skill], ' ' * (20 - len(str(total_count_dict[skill]))),
-                      rate_winner_side,
-                      rate_occ_two_side_total, ' ' * (26 - len(str(rate_occ_two_side_total)))
+                      rate_winner_side, ' ' * (18 - len(str(rate_winner_side))),
+                      rate_occ_two_side_total
                       ))
 
     return dict_for_plot
